220dead943b518c82529a84b55d809fb9d73a776
Alexander <pircuser61@rambler.ru>
2017-03-04 02:19:16 +0300

    авторизация

--- test_app/views.py
+++ test_app/views.py
@@ -1,28 +1,58 @@
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
-
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 class Main(generic.TemplateView):
+    template_name = 'main.html'
 
-    def get(self, request, *args, **kwargs):
-        context = dict()
-        context['generic'] = True
-        if request.user.is_authenticated:
-            context['user_name'] = request.user.username
-            self.template_name = 'user.html'
+    def get_context_data(self, **kwargs):
+        context = super(generic.TemplateView, self).get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context['user_name'] = self.request.user.username
         else:
-            self.template_name = 'main.html'
+            context['login_form'] = LoginForm(initial={'redirect_url':self.request.path})
+        return context
 
-        return self.render_to_response(context)
+
+class LoginForm(forms.Form):
+    login_ = forms.CharField(label='Логин')
+    pass_ = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    redirect_url = forms.CharField() #widget=forms.HiddenInput()
 
 
-class Login(generic.View):
+
+class Login2(generic.FormView):
+    form_class = LoginForm
+    template_name = 'login_form.html'
+
+
+    def form_valid(self, form):
+        login_ = form.cleaned_data['login_']
+        pass_ = form.cleaned_data['pass_']
+        redirect_url = form.cleaned_data['redirect_url']
+        user = authenticate(username=login_, password=pass_)
+        if user:
+            login(self.request, user)
+            return redirect(redirect_url)
+        return redirect('/err')  # обработать неправильные данные
+
+    def get_initial(self):
+        """
+        Returns the initial data to use for forms on this view.
+        """
+        return {'redirect_url': '/'}
+
+
+class Login1(generic.View):
 
     def post(self, request, *args, **kwargs):
-        login_ = request.POST['login']
-        pass_ = request.POST['password']
-        url_ = request.POST['url']
+        login_ = request.POST['login_']
+        pass_ = request.POST['pass_']
+        url_ = request.POST['redirect_url']
+        print(self.request.path)
         user = authenticate(username=login_, password=pass_)
         if user:
             login(request, user)
